e057334c7af2ddcdd1110e1075f36fca22c790e3
Paulo Costa <=>
2015-12-08 23:01:50 -0300

    add some changes...

--- js/app.js
+++ js/app.js
@@ -4,36 +4,34 @@ var app = angular.module('myApp', []);
 app.controller('cidadeCtrl', function($scope, $http) {
 
 	$scope.cidade =  [];
-	$scope.temp = [];
+	$scope.unidade = [];	
 
   	$http.get("all.json")
     	.then(function (response) {
-      	//console.log(response.data[0]);
          $scope.cidade = response.data;
-          //console.log($scope.cidade);
-        //$scope.names = response.data.records
     });
 
    	$scope.getUnidade = function(){
+   		// remove lista quando selecionar novo cidade
+   		$scope.unidade = [];
+
    		var key = this.data.id;
-		$scope.unidade = [];
 
 	    $http.get("unidade.json")
 	    	.then(function (response){
 	    		for (var i = 0; i < response.data.length; i++) {
-	    			console.log(response.data[i].nome);
-	    			console.log(key);
-	    			console.log(response.data[i].cidade_id);
-	    			if(response.data[i].cidade_id === key){
-							console.log(response.data[i].nome);
-	    			}else{
-	    				console.log("error");
+	    			if(response.data[i].cidade_id == key){
+							$scope.unidade.push(response.data[i]);
 	    			}
-	    				
 	    		}
+	    		
 	    		console.log($scope.unidade);
 	    });
 
-   	}
+   	};
+
+   	$scope.getDetails = function(){
+   		console.log(this.data);
+   	};
 
 });
